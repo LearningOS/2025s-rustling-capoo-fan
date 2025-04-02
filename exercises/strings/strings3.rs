5079ac4c17cf8ecb31b9b0a9c51711c0f368f6cc
qianfu <1733034367@qq.com>
2025-04-02 14:03:06 +0800

    string类型完成

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,25 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let s = input.trim().to_string();//去除空字符的函数
+    s
 }
-
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut s: String = input.to_string();
+    s += " world!";
+    s
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let s = input.replace("cars", "balloons").to_string();//替换函数，返回str，需要转换成String
+    s
+    
 }
 
 #[cfg(test)]
